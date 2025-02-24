4ae874c5228e4ed0cf58a6caa5507a7f5339dd59
Orange <Orange@DESKTOP-3UUB6RH>
2025-02-24 04:41:59 +0300

    V06

---
+++ Task_06/Task_06/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_06.Models;
+
+public partial class Department
+{
+    public int Id { get; set; }
+
+    public string DepName { get; set; } = null!;
+
+    public string? ManagerName { get; set; }
+
+    public decimal? Budget { get; set; }
+
+    public DateOnly? EstablishedDate { get; set; }
+}
---
+++ Task_06/Task_06/Models/MyDbContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Task_06.Models;
+
+public partial class MyDbContext : DbContext
+{
+    public MyDbContext()
+    {
+    }
+
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-3UUB6RH; Database=Orange; Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Departme__3214EC2790B2940E");
+
+            entity.ToTable("Department");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Budget).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.DepName).HasMaxLength(100);
+            entity.Property(e => e.ManagerName).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC275EC0AC90");
+
+            entity.HasIndex(e => e.Email, "UQ__Users__A9D10534108DCA79")
+                .IsUnique()
+                .HasFilter("([Email] IS NOT NULL)");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Name).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
